10ee78396199e6dd1a4ab9687f955e30e6b92713
George Williamson <georgekwilliamson@gmail.com>
2020-08-30 21:24:26 -0500

    works

--- src/App.js
+++ src/App.js
@@ -1,3 +1,39 @@
+// import React, { useState, useEffect } from 'react';
+// import './App.css';
+// import { API } from 'aws-amplify'
+
+
+
+// function App() {
+
+//   const [coins, setCoins] = useState([]);
+
+//   async function fetchCoins() {
+//     const data = await API.get('cryptoapi', '/coins')
+//     setCoins(data.coins)
+//   }
+
+//   useEffect(() => {
+//     fetchCoins()
+
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       {
+//         coins.map((coin, index) => (
+//           <div key={index}>
+//             <h2>{coin.name} - {coin.symbol}</h2>
+//             <h5>${coin.price_usd}</h5>
+//           </div>
+//         ))
+//       }
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API } from 'aws-amplify'
@@ -6,20 +42,35 @@ import { API } from 'aws-amplify'
 
 function App() {
 
+  const [input, setInput] = useState({ limit: 5, start: 0 });
   const [coins, setCoins] = useState([]);
 
   async function fetchCoins() {
-    const data = await API.get('cryptoapi', '/coins')
+    const { limit, start } = input
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`)
+    // const data = await API.get('cryptoapi', `/coins`)
     setCoins(data.coins)
+  }
+
+  function updateInputValues(type, value) {
+    setInput({ ...input, [type]: value })
   }
 
   useEffect(() => {
     fetchCoins()
-
   }, []);
 
   return (
     <div className="App">
+      <input
+        onChange={e => updateInputValues('limit', e.target.value)}
+        placeholder="limit"
+      />
+      <input
+        onChange={e => updateInputValues('start', e.target.value)}
+        placeholder="start"
+      />
+      <button onClick={fetchCoins}>Fetch Coins</button>
       {
         coins.map((coin, index) => (
           <div key={index}>
@@ -33,3 +84,4 @@ function App() {
 };
 
 export default App;
+
